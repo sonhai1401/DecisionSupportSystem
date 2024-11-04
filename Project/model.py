603eedda1907a91e976ddca1105d6c452041b9ed
sonhai1401 <146439497+sonhai1401@users.noreply.github.com>
2024-11-04 19:20:07 +0700

    Update model.py

--- Project/model.py
+++ Project/model.py
@@ -30,6 +30,7 @@ y = data_encoded['Số lượng Nhập kho']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Chuẩn hóa các đặc trưng
+# Công thức StandardScaler (xi-mean(x))/(stdev(x))
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
